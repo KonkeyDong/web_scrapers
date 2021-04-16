1344c1769a6e8db5d038b76cb73b784d81e1a828
konkeydong <dittoes.1@gmail.com>
2021-04-16 00:15:20 +0000

    DO NOT PUSH

--- manga/config.rb
+++ manga/config.rb
@@ -2,7 +2,7 @@
 # Using Dragon Ball, for example, means all chapters for Dragon Ball will be saved under:
 # /base/directory/path/to/store/your/books/dragon_ball
 # Change this path to your liking.
-BASE_DIRECTORY_PATH = '/base/directory/path/to/store/your/books'
+BASE_DIRECTORY_PATH = "/media/HDD_4TB_01/manga"
 
 READ_TIMEOUT = 30 # in seconds
 
@@ -14,5 +14,37 @@ READ_TIMEOUT = 30 # in seconds
 # Here are some examples of how to use this:
 URL_DATA = [
     ["http://www.tenmanga.com/book/Dragon+Ball.html", "dragon_ball"],
-    ["http://www.tenmanga.com/book/Attack+On+Titan+-+Before+The+Fall.html", "attack_on_titan"],
+    ["http://www.tenmanga.com/book/Attack+On+Titan+-+Before+The+Fall.html", "attack_on_titan_-_before_the_fall"],
+    ["http://www.tenmanga.com/book/Yu+Yu+Hakusho.html", "yu_yu_hakusho"],
+    ["http://www.tenmanga.com/book/MONSTER+MUSUME+NO+IRU+NICHIJOU.html?waring=1", "monster_museum"],
+    ["http://www.tenmanga.com/book/God+of+Martial+Arts.html", "god_of_martial_arts"],
+    ["http://www.tenmanga.com/book/Kaifuku+Jutsushi+no+Yarinaoshi.html", "redo_of_healer"],
+    ["http://www.tenmanga.com/book/%2BC+Sword+and+Cornett.html", "+c_sword_and_cornett"],
+    ["http://www.tenmanga.com/book/NANATSU+NO+TAIZAI.html", "seven_deadly_sins"],
+    ["http://www.tenmanga.com/book/BLACK+CLOVER.html", "black_clover"],
+    ["http://www.tenmanga.com/book/666+Satan.html", "666_satan"],
+    ["http://www.tenmanga.com/book/Star+Martial+God+Technique.html", "star_martial_god_technique"],
+    ["http://www.tenmanga.com/book/HISTORY%27S+STRONGEST+DISCIPLE+KENICHI.html", "historys_strongest_disciple_kenichi"],
+    ["http://www.tenmanga.com/book/Berserk.html", "berserk"],
+    ["http://www.tenmanga.com/book/Tokyo+Ghoul.html", "tokyo_ghoul"],
+    ["http://www.tenmanga.com/book/Tokyo+Ghoul%3Are.html", "tokyo_ghoul_re"],
+    ["http://www.tenmanga.com/book/Assassination+Classroom.html", "assassination_classroom"],
+    ["http://www.tenmanga.com/book/TERRA+FORMARS.html?waring=1", "terra_formars"],
+    ["http://www.tenmanga.com/book/ONEPUNCH-MAN.html", "onepunch_man"],
+    ["http://www.tenmanga.com/book/Overlord.html", "overlord"],
+    ["http://www.tenmanga.com/book/DOULUO+DALU+II+-+JUESHUI+TANGMEN.html", "douluo_dalu_II"],
+    ["http://www.tenmanga.com/book/07+Ghost.html", "07_ghost"],
+    ["http://www.tenmanga.com/book/Hunter+X+Hunter.html", "hunter_x_hunter"],
+    ["http://www.tenmanga.com/book/TOWER+OF+GOD.html", "tower_of_god"],
+    ["http://www.tenmanga.com/book/Naruto.html", "naruto"],
+    ["http://www.tenmanga.com/book/One+Piece.html", "one_piece"],
+    ["http://www.tenmanga.com/book/Ijiranaide%2C+Nagatoro-san.html", "please_do_not_bully_me_nagatoro"],
+    ["http://www.tenmanga.com/book/Bleach.html", "bleach"],
+    ["http://www.tenmanga.com/book/Prison+School.html?waring=1", "prison_school"],
+    ["http://www.tenmanga.com/book/Solo+Leveling.html", "solo_leveling"],
+    ["http://www.tenmanga.com/book/CHANGE+GUY.html", "change_guy"],
+    ["http://www.tenmanga.com/book/Tensei+Shitara+Slime+Datta+Ken.html", "tensei"],
+    ["http://www.tenmanga.com/book/Tenchi+Muyo%21.html", "tenchi_muyo"],
+    ["http://www.tenmanga.com/book/High+School+of+the+Dead.html?waring=1", "high_school_of_the_dead"],
 ]
+
